76704b47cef71149d8c2a4871008a17a5f95ce3e
Huang <huangche@406c8f566eb4.ant.amazon.com>
2014-04-19 16:20:21 -0700

    add paymentInfo

---
+++ src/com/app/billsplitapp/PaymentInfo.java
@@ -0,0 +1,10 @@
+package com.app.billsplitapp;
+
+/**
+ * Created by huangche on 4/18/14.
+ */
+public class PaymentInfo {
+    public String title;
+    public String email;
+    public double amount;
+}
